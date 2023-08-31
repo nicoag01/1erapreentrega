2ed489a33946204353abb589286acbec0e06d7b5
nicolas garcia <nagarcia@gmail.com>
2023-08-31 19:53:39 -0300

    1era Pre entrega

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import ItemListContainer from "./components/ItemListContainer"
+import NavBar from "./components/NavBar"
+
+const App = () => {
+
+  return (
+    <>
+      <NavBar />
+      <ItemListContainer greeting={"Bienvenidos a la mejor tienda de ropa deportiva"}/>
+    </>
+  )
+}
+
+export default App
